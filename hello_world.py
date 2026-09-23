b88a180a218a76084fa074504e4e4e9d5245e077
Rohit Singh <rohitsinghg20@gmail.com>
2026-09-23 16:23:02 +0530

    Basic program

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+print("This is a simple Python program that prints 'Hello, World!' to the console.")
+def int(n):
+    print(n)
+
+int(5)
---
+++ int_method.py
@@ -0,0 +1,3 @@
+from hello_world import int
+
+int(5)
